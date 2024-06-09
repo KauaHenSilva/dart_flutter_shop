fb05fc96edb6d89493cc6474cce261dd96877d8a
KauaHenSilva <kaua.sbc@gmail.com>
2024-06-09 14:27:27 -0300

    feat: Update ProductList to use MyConst for URL and remove unused imports

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -1,18 +1,18 @@
 import 'dart:convert';
+import 'package:dart_flutter_shop/const/my_const.dart';
 import 'package:flutter/material.dart';
 import 'product_model.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
 class ProductList extends ChangeNotifier {
   final List<ProductModel> _products = [];
-  final urlFireBase = dotenv.get('FIREBASE_URL');
 
   int get productLen => _products.length;
   List<ProductModel> get products => [..._products];
 
   Future<void> getProducts() async {
-    final resp = await http.get(Uri.parse('$urlFireBase/products.json'));
+    final resp = await http.get(Uri.parse('${MyConst().urlProducts}.json'));
+
     if (resp.body == 'null') return;
 
     if (resp.statusCode >= 400) throw Exception('Failed to load products');
