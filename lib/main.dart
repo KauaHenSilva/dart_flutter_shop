2faec3b0fed2dc7141b8d23061a2701ca0b0b4f2
KauaHenSilva <kaua.sbc@gmail.com>
2024-06-09 09:28:21 -0300

    init project

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'screens/home_page.dart';
+import 'utils/my_routes.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Shopping App',
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
+      routes: {
+        MyRoutes.home: (context) => const HomePage(),
+      },
+    );
+  }
+}
