d3e830a338612f5b3d8ff5be1ab2b925509d8f07
KauaHenSilva <kaua.sbc@gmail.com>
2024-06-15 10:29:57 -0300

    feat: add logo and splash

--- lib/const/my_const.dart
+++ lib/const/my_const.dart
@@ -1,12 +1,7 @@
-import 'package:flutter/material.dart';
-
 class MyConst {
   final url = const String.fromEnvironment('URL_BASE');
 
-  String get urlProducts {
-    debugPrint('URL_BASE: $url');
-    return '$url/products';
-  }
+  String get urlProducts => '$url/products';
   String get urlCart => '$url/cart';
   String get urlOrders => '$url/orders';
 }
